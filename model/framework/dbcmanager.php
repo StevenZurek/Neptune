f59db7ddf5dfab21ad70a04eb9c5d8196055ac4b
Derek <derek.adair@azimuth360.com>
2011-07-26 11:39:34 -0600

    DBC connection debugging BS

--- model/framework/dbcmanager.php
+++ model/framework/dbcmanager.php
@@ -1,25 +1,30 @@
 <?php
+
 function mysqliConnect($configFile) {
   //Load the database settings file  
   $settings = simplexml_load_file($configFile);
   //Validate the settings.xml file
+  $errMsg = "";
   if ((string) $settings->mysql->username === "_USER_") {
-    $errMsg = "Please set the database username (./Neptune/model/framework/settings.xml\n";
+    $errMsg .= "Please set the database <i>username</i> (./Neptune/model/framework/settings.xml <br />";
   }
   if ((string) $settings->mysql->password === "_PASSWORD_") {
-    $errMsg = "Please set the database password (./Neptune/model/framework/settings.xml\n";
+    $errMsg .= "Please set the database <i>password</i> (./Neptune/model/framework/settings.xml <br />";
   }
   if ((string) $settings->mysql->database === "_DATABASE_") {
-    $errMsg = "Please set the database name (./Neptune/model/framework/settings.xml\n";
+    $errMsg .= "Please set the database <i>name</i>(./Neptune/model/framework/settings.xml <br />";
   }
 
-  if (isset($errMsg)) {
-    printf("Neptune Error:" + $errMsg);
+  if ($errMsg !== "") {
+    printf("<h1>Neptune Error: </h1><p>" . $errMsg . '</p>');
     exit();
   }
 
   $con = mysqli_connect(
-          (string) $settings->mysql->server, (string) $settings->mysql->username, (string) $settings->mysql->password, (string) $settings->mysql->database);
+          (string) $settings->mysql->server, 
+          (string) $settings->mysql->username, 
+          (string) $settings->mysql->password, 
+          (string) $settings->mysql->database);
 
   if (mysqli_connect_errno()) {
     printf("Connection failed: %s\n", mysqli_connect_error());
@@ -37,4 +42,5 @@ class mysqlRow {
   }
 
 }
+
 ?>
